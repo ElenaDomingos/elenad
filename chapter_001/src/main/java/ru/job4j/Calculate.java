e01e4b5faa7632a8a1e1ed5c6bfaba2eca310e2a
Elena Domingos <elena-lezhneva@mail.ru>
2016-12-05 10:31:21 -0200

    Update Calculate.java

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**
+*Class Calulate подключение сборщика Maven
+*@autor Elena Domingos
+*@since 12.04.2016
+*/
+
 public class Calculate {
+    
+/**
+*@param value Строка для вывода в консоль
+*@return String value
+*/
+
     public String echo(String value) {
         
         return String.format("%s %s %s", value, value, value);
