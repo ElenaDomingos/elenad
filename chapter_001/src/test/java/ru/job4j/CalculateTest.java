e1df69944f62649fbd7b4917bc130581110fb900
Elena Domingos <elena-lezhneva@mail.ru>
2016-12-06 15:38:38 -0200

    Update CalculateTest.java

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -1,18 +1,29 @@
 package ru.job4j;
 
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 
 public class CalculateTest {
+    /**
+    *Test add.
+    */
+    
     @Test
-    public void whenSetStopInEchoThenReturnThreeStops() {
+public void whenAddOneToOneThenTwo() { {
        
-        Calculate calc = new Calculate();
-        String result = calc.echo("stop");
-        assertThat(result, is("stop stop stop"));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(out.toString(), is("Hello World\r\n"));
+    }
+
+    
+        
     }
     
- }
+}
    
