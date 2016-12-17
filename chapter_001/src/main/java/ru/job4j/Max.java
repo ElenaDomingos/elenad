354a0980025232ddd5302da7471e9731707335cc
ElenaDomingos <elena-lezhneva@mail.ru>
2016-12-17 18:35:19 -0200

    add max.java

---
+++ chapter_001/src/main/java/ru/job4j/Max.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+public class Max {
+	
+	public Max(int first, int second) {
+		
+		System.out.println(first > second ? "First is max value" : "Second is max value");
+		
+		
+	}
+	
+	
+	
+	
+}
