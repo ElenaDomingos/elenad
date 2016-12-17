e9abe897c70bb87522fcf3d6bb8f61f9d4d7f4fc
ElenaDomingos <elena-lezhneva@mail.ru>
2016-12-17 18:37:40 -0200

    add maxtest.java

---
+++ chapter_001/src/test/java/ru/job4j/MaxTest.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class PointTest {
+    /**
+    *Test add.
+    */
+    
+@Test
+public void WhenFirstIsBiggerThanShowFirstIsMax() { 
+
+				first = 4;
+				second = 2;
+				assertThat(Max(4,2), is("First is max value"));
+				
+				
+}
+
+}
