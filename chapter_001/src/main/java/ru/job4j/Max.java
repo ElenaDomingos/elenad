cde9727138e6f47719adcb6babb62fa92b135484
Elena Domingos <elena-lezhneva@mail.ru>
2016-12-20 11:17:40 -0200

    Create Max.java

--- chapter_001/src/main/java/ru/job4j/Max.java
+++ chapter_001/src/main/java/ru/job4j/Max.java
@@ -2,18 +2,18 @@ package ru.job4j;
 
 public class Max {
 	
-	public max(int first, int second) {
+		public int max(int first, int second) {
 		
 		int max = 0;		
 		(first > second) ? max = first : max = second;
-		
+		return max;
 		
 	}
 	
-	public max(int first, int second, int third) {
+	public int max(int first, int second, int third) {
 		
-		(third > max) ? max = third : max = max;
-		
+		third > max) ? max = third : max = max;
+		return max;
 		
 	}
 	
