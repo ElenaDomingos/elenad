65142a81e8c8202b4e719494de307f7aaaa712d6
Elena Domingos <elena-lezhneva@mail.ru>
2016-12-20 13:09:51 -0200

    Update Max.java

--- chapter_001/src/main/java/ru/job4j/Max.java
+++ chapter_001/src/main/java/ru/job4j/Max.java
@@ -2,11 +2,11 @@ package ru.job4j;
 
 public class Max {
 	
-<<<<<<< HEAD
-	public int max(int first, int second) {
-=======
+
+	
+
 		public int max(int first, int second) {
->>>>>>> a0116ba5fe9ed205ea0c6d82478b8f896eba1b54
+
 		
 		int max = 0;		
 		(first > second) ? max = first : max = second;
