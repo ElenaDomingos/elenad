e6a275099f1fc8d7bef87d71ab792607b332f7db
ElenaDomingos <elena-lezhneva@mail.ru>
2016-12-27 14:31:06 -0200

    add turntest.java

---
+++ chapter_001/src/test/java/ru/job4j/TurnTest.java
@@ -0,0 +1,23 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TurnTest {
+    /**
+    *Test add.
+    */
+    
+@Test
+public void WhenArrayIsExistThanShowResult() { 
+
+				
+				assertThat(resultArray, is(expectArray));
+				
+				
+}
+
+}
