96963459838a9518700c68aa079d306ee70fc4b3
ElenaDomingos <elena-lezhneva@mail.ru>
2016-12-27 14:19:37 -0200

    add turn.java

---
+++ chapter_001/src/main/java/ru/job4j/Turn.java
@@ -0,0 +1,30 @@
+package ru.job4j;
+
+public class Turn {
+	//public static void main(String[] args){
+	//int[] values = new int[5];
+	//values[0] =1;
+	//values[1] =13;
+	//values[2] =5;
+	//values[3] =6;
+	//values[4] =6;
+	
+	
+	//Turn arr = new Turn();
+    //    arr.back(values);
+       
+
+	//}
+
+    public int back(int[] values) {
+
+	    
+        for(int index = values.length-1; index>=0; index--) {
+	    System.out.print(values[index]+" ");
+		
+	
+         }
+      return 0;
+     }
+
+}
