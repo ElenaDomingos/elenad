a47ad28fcf79cdb3df56d905ec507ecdf8b4291d
ElenaDomingos <elena-lezhneva@mail.ru>
2017-01-14 03:02:51 -0200

    add boobletest.java

---
+++ chapter_001/src/test/java/ru/job4j/BoobleTest.java
@@ -0,0 +1,23 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BoobleTest {
+    /**
+    *Test add.
+    */
+    
+@Test
+public void WhenArrayIsExistThanShowResult() { 
+
+				
+				assertThat(resultArray, is(expectArray));
+				
+				
+}
+
+}
