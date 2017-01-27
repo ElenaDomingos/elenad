66c0da9a678011be67e17e2cd8d1cc52f6972624
ElenaDomingos <elena-lezhneva@mail.ru>
2017-01-26 22:45:29 -0200

    add removeduplicate.java

--- chapter_001/src/main/java/ru/job4j/RemoveDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/RemoveDuplicate.java
@@ -1,8 +1,21 @@
-
+package ru.job4j;
 /**
  * Created by Helena on 14/01/2017.
  */
 public class RemoveDuplicate {
+	public static void main(String[] args){
+		
+		String arr[] = new String[] {"Liza", "Olga", "Liza", "Sveta","Valeria"};
+		
+		RemoveDuplicate newarr = new RemoveDuplicate();
+		newarr.removeDuplicates(arr);
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+		
+		
+	}
 
     public static void removeDuplicates(String[] list) {
         for (int i = 0; i < list.length; i++)
@@ -10,7 +23,7 @@ public class RemoveDuplicate {
             String a = list[i];
             for(int j = 0; j< list.length; )
 
-                if (a == list[j])
+                if (a.equals(list[j]))
                 {
                     list[j] = null;
                     list[j] = list[list.length - j]; }
