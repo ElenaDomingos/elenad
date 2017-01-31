aceca562b3dcc7d1cbcc00b9c179b8ebe086d1b9
ElenaDomingos <elena-lezhneva@mail.ru>
2017-01-30 23:12:00 -0200

    removeduplicate.java

--- chapter_001/src/main/java/ru/job4j/RemoveDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/RemoveDuplicate.java
@@ -3,34 +3,42 @@ package ru.job4j;
  * Created by Helena on 14/01/2017.
  */
 public class RemoveDuplicate {
-	public static void main(String[] args){
-		
-		String arr[] = new String[] {"Liza", "Olga", "Liza", "Sveta","Valeria"};
-		
-		RemoveDuplicate newarr = new RemoveDuplicate();
-		newarr.removeDuplicates(arr);
-		
-		for(int i=0;i<arr.length;i++) {
-			System.out.println(arr[i]);
-		}
-		
-		
-	}
 
-    public static void removeDuplicates(String[] list) {
-        for (int i = 0; i < list.length; i++)
+    public static void main(String[] args){
+
+
+        String[] arr =  {"Liza", "Olga", "Liza", "Sveta", "Valeria"};
+
+       removeDuplicates(arr);
+
+        for(int i=0; i < arr.length ; i++) {
+           System.out.println(arr[i]);
+        }
+
+
+    }
+
+    public static void removeDuplicates(String[] arr) {
+
+
+        for (int i = 0; i <arr.length; i++)
         {
-            String a = list[i];
-            for(int j = 0; j< list.length; )
+            String a = arr[i];
+            for(int j = 0; j < arr.length; )
 
-                if (a.equals(list[j]))
-                {
-                    list[j] = null;
-                    list[j] = list[list.length - j]; }
-                else {
+            {
+                if (a.equals(arr[j])) {
+                    
+                     arr[j]= null;
+                    arr[j] = arr[arr.length-1];
+					j++;
+
+                } else {
                     j++;
 
                 }
+            }
+
         }
     }
 
