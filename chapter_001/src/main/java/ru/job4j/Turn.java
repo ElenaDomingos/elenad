c297d220b36a5adcb560ecf0328a1640a5c883dd
ElenaDomingos <elena-lezhneva@mail.ru>
2017-02-02 14:49:26 -0200

    add turn.java

--- chapter_001/src/main/java/ru/job4j/Turn.java
+++ chapter_001/src/main/java/ru/job4j/Turn.java
@@ -1,30 +1,39 @@
 package ru.job4j;
 
 public class Turn {
-	//public static void main(String[] args){
-	//int[] values = new int[5];
-	//values[0] =1;
-	//values[1] =13;
-	//values[2] =5;
-	//values[3] =6;
-	//values[4] =6;
+	public static void main(String[] args){
+	int[] values = new int[5];
+	values[0] =1;
+	values[1] =13;
+	values[2] =5;
+	values[3] =6;
+	values[4] =6;
 	
-	
-	//Turn arr = new Turn();
-    //    arr.back(values);
+	     
+        back(values);
+       for(int i =0; i<values.length; i++) {
+            System.out.println(values[i]);
+       }
        
 
-	//}
+	}
 
-    public int back(int[] values) {
+    public static int back(int[] values) {
 
-	    
-        for(int index = values.length-1; index>=0; index--) {
-	    System.out.print(values[index]+" ");
-		
-	
-         }
-      return 0;
+	    int[] arr = new int[5];
+        for(int index = 0; index<values.length; index++) {
+			
+			
+			arr[index] = values[values.length-index-1] ;
+		}
+        
+		for(int i=0; i<values.length;i++){
+			values[i] = arr[i];
+		}
+    
+				
+	      
+     return 0;
      }
 
 }
