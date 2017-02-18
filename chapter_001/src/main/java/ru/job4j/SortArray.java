34798905a33b6e46af70a68d235c0823f85b1568
ElenaDomingos <elena-lezhneva@mail.ru>
2017-02-18 06:10:55 -0200

    add sortarray.java

--- chapter_001/src/main/java/ru/job4j/SortArray.java
+++ chapter_001/src/main/java/ru/job4j/SortArray.java
@@ -19,7 +19,7 @@ public static void main(String[] args) {
 int[] Sortornot = new int[5];	
 Sortornot[0]=1;
 Sortornot[1]=2;
-Sortornot[2]=3;
+Sortornot[2]=7;
 Sortornot[3]=4;
 Sortornot[4]=5;
 
@@ -36,14 +36,11 @@ public static void IfSort(int[] arr) {
 	
 	for(int i=0; i < arr.length-1;i++){
 		
-		for(int j=1;j<arr.length; j++) {
-			
-			if (arr[i] < arr[j]) {
+		if (arr[i] < arr[i+1]) {
 			
 			allsort = true;
 			
-            			
-		    }
+           }
             
             else {
                 allsort = false;
@@ -51,9 +48,7 @@ public static void IfSort(int[] arr) {
                 break;
 				
                 
-            }
-            
-                    
+                 
         }
 	
 	}
