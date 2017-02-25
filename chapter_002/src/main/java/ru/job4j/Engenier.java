bdaf45f9f86ea47b4b423225e1a23f38ec8f090f
ElenaDomingos <elena-lezhneva@mail.ru>
2017-02-25 16:25:20 -0300

    add engenier.java

---
+++ chapter_002/src/main/java/ru/job4j/Engenier.java
@@ -0,0 +1,16 @@
+package ru.job4j;
+
+
+public class Engenier extends Professions{
+
+public static void main(String[] args) {
+	
+Engenier egor = new Engenier();
+
+egor.getSalaryAfterTaxesWasPaid(1250, 100);	
+	
+}
+
+	
+}
+
