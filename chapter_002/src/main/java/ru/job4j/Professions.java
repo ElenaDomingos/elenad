64c3ab7f23610dad1a20d5adbc7fb9ed38e99c32
ElenaDomingos <elena-lezhneva@mail.ru>
2017-02-25 16:23:37 -0300

    add professions.java

---
+++ chapter_002/src/main/java/ru/job4j/Professions.java
@@ -0,0 +1,36 @@
+package ru.job4j;
+
+
+public class Professions {
+	
+String name;
+
+boolean education;
+	
+int experienceyears;
+
+int hoursperday;
+
+int salary;
+
+int taxespaid;
+
+
+public void work(int hoursperday) {
+	
+	for(int i = 0;i < hoursperday; i++) {
+		
+		System.out.println("Is working");
+		
+	}
+	
+}	
+
+public void getSalaryAfterTaxesWasPaid(int salary, int taxespaid){
+	
+int lucro = salary - taxespaid;
+System.out.print(lucro);
+
+}
+	
+}
